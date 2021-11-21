dbb41904cd1f021f3f6f7882eb7fb065640e9535
Joshua Peek <josh@joshpeek.com>
2021-11-20 20:21:14 -0800

    Add table

---
+++ data.sql
@@ -0,0 +1,11 @@
+PRAGMA foreign_keys=OFF;
+BEGIN TRANSACTION;
+CREATE TABLE IF NOT EXISTS "movies" (
+  "wikidata" text,
+  "imdb" text
+);
+INSERT INTO movies VALUES('Q47703','tt0068646');
+INSERT INTO movies VALUES('Q172241','tt0111161');
+CREATE UNIQUE INDEX wikidata ON "movies" ("wikidata" ASC);
+CREATE UNIQUE INDEX imdb ON "movies" ("imdb" ASC);
+COMMIT;
