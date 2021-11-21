fa05fb93bf5e654369636184cf9b9485babb644c
Joshua Peek <josh@joshpeek.com>
2021-11-20 22:10:24 -0800

    Rename to items

--- data.sql
+++ data.sql
@@ -1,13 +1,15 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "movies" (
+CREATE TABLE IF NOT EXISTS "items" (
   "wikidata" text,
-  "imdb" text
+  "imdb" text,
+  "tmdb" text
 );
-INSERT INTO movies VALUES('Q47703','tt0068646');
-INSERT INTO movies VALUES('Q172241','tt0111161');
-INSERT INTO movies VALUES('Q732960','tt0780504');
-INSERT INTO movies VALUES('Q60834962',NULL);
-CREATE UNIQUE INDEX wikidata ON "movies" ("wikidata" ASC);
-CREATE UNIQUE INDEX imdb ON "movies" ("imdb" ASC);
+INSERT INTO items VALUES('Q47703','tt0068646',NULL);
+INSERT INTO items VALUES('Q172241','tt0111161',NULL);
+INSERT INTO items VALUES('Q732960','tt0780504',NULL);
+INSERT INTO items VALUES('Q60834962',NULL,NULL);
+CREATE UNIQUE INDEX wikidata ON "items" ("wikidata" ASC);
+CREATE UNIQUE INDEX imdb ON "items" ("imdb" ASC);
+CREATE UNIQUE INDEX tmdb ON "items" ("tmdb" ASC);
 COMMIT;
