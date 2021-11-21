451569d7b815ff2dc1f06d80356eed391f59bf22
Joshua Peek <josh@joshpeek.com>
2021-11-20 21:37:56 -0800

    Use indexes from env var

--- export_db.py
+++ export_db.py
@@ -6,27 +6,15 @@ import sys
 con = sqlite3.connect("file:" + sys.argv[1] + "?mode=ro", uri=True)
 con.row_factory = sqlite3.Row
 
-index_names = set()
-
-for result in con.execute("PRAGMA index_list('movies')"):
-    if result["unique"] == 1:
-        index_names.add(result["name"])
-
-index_info = {}
-
-for name in index_names:
-    results = con.execute("PRAGMA index_info('" + name + "')")
-    cols = index_info[name] = [result["name"] for result in results]
-
 root = sys.argv[2]
+
+index_names = os.environ["INDEXES"].split()
 
 for result in con.execute("SELECT * FROM movies"):
     row = dict(result)
 
-    for index_name in index_info:
-        index_col_name = index_info[index_name][0]
-        assert index_col_name == index_name
-        filename = "{}.json".format(result[index_col_name])
+    for index_name in index_names:
+        filename = "{}.json".format(result[index_name])
         filename = os.path.join(root, index_name, filename)
 
         with open(filename, "w") as f:
