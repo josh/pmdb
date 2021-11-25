61778580c8bfa672d380328bc823440faaf0f40d
Joshua Peek <josh@joshpeek.com>
2021-11-24 17:41:09 -0800

    Add title column

--- data.sql
+++ data.sql
@@ -1,15 +1,16 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS 'items' (
-  'wikidata' text,
-  'imdb' text,
-  'tmdb' integer
+CREATE TABLE IF NOT EXISTS "items" (
+  "wikidata" text,
+  "imdb" text,
+  "tmdb" integer,
+  "title" text
 );
-INSERT INTO items VALUES('Q172241','tt0111161',278);
-INSERT INTO items VALUES('Q47703','tt0068646',238);
-INSERT INTO items VALUES('Q732960','tt0780504',64690);
-INSERT INTO items VALUES('Q60834962','tt1160419',438631);
-CREATE UNIQUE INDEX tmdb ON 'items' ('tmdb' ASC);
-CREATE UNIQUE INDEX imdb ON 'items' ('imdb' ASC);
-CREATE UNIQUE INDEX wikidata ON 'items' ('wikidata' ASC);
+INSERT INTO items VALUES('Q172241','tt0111161',278,NULL);
+INSERT INTO items VALUES('Q47703','tt0068646',238,NULL);
+INSERT INTO items VALUES('Q732960','tt0780504',64690,NULL);
+INSERT INTO items VALUES('Q60834962','tt1160419',438631,NULL);
+CREATE UNIQUE INDEX wikidata ON "items" ("wikidata" ASC);
+CREATE UNIQUE INDEX imdb ON "items" ("imdb" ASC);
+CREATE UNIQUE INDEX tmdb ON "items" ("tmdb" ASC);
 COMMIT;
