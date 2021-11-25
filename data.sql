dd382cdb9e46651dc6508c2b36754be011aaa267
Joshua Peek <josh@joshpeek.com>
2021-11-24 16:07:32 -0800

    Update data

--- data.sql
+++ data.sql
@@ -1,15 +1,15 @@
 PRAGMA foreign_keys=OFF;
 BEGIN TRANSACTION;
-CREATE TABLE IF NOT EXISTS "items" (
-  "wikidata" text,
-  "imdb" text,
-  "tmdb" text
+CREATE TABLE IF NOT EXISTS 'items' (
+  'wikidata' text,
+  'imdb' text,
+  'tmdb' integer
 );
-INSERT INTO items VALUES('Q47703','tt0068646',NULL);
 INSERT INTO items VALUES('Q172241','tt0111161',NULL);
+INSERT INTO items VALUES('Q47703','tt0068646',NULL);
 INSERT INTO items VALUES('Q732960','tt0780504',NULL);
-INSERT INTO items VALUES('Q60834962',NULL,NULL);
-CREATE UNIQUE INDEX wikidata ON "items" ("wikidata" ASC);
-CREATE UNIQUE INDEX imdb ON "items" ("imdb" ASC);
-CREATE UNIQUE INDEX tmdb ON "items" ("tmdb" ASC);
+INSERT INTO items VALUES('Q60834962','tt1160419',438631);
+CREATE UNIQUE INDEX tmdb ON 'items' ('tmdb' ASC);
+CREATE UNIQUE INDEX imdb ON 'items' ('imdb' ASC);
+CREATE UNIQUE INDEX wikidata ON 'items' ('wikidata' ASC);
 COMMIT;
