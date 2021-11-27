476cfe09b48dd2ba77efda2cacb71ced25820788
Joshua Peek <josh@joshpeek.com>
2021-11-26 16:11:29 -0800

    More tests

--- test_export_database.py
+++ test_export_database.py
@@ -1,4 +1,4 @@
-from export_database import clean_dict
+from export_database import clean_dict, get_value
 
 
 def test_clean_dict():
@@ -10,3 +10,11 @@ def test_clean_dict():
 
     assert d2["foo"] == 42
     assert "bar" not in d2
+
+
+def test_get_value():
+    obj = {"foo": 1, "bar": {"baz": 2, "qux": {"quux": 3}}}
+    assert get_value(obj, ["foo"]) == 1
+    assert get_value(obj, ["bar", "baz"]) == 2
+    assert get_value(obj, ["bar", "qux"]) == {"quux": 3}
+    assert get_value(obj, ["bar", "qux", "quux"]) == 3
