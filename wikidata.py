d64e0cc169e045b508df091bf1db50e1c2bd223d
Joshua Peek <josh@joshpeek.com>
2021-12-04 15:29:33 -0800

    Tidy vars

--- wikidata.py
+++ wikidata.py
@@ -170,44 +170,44 @@ media_properties = {
 
 
 def fetch_media_items(qids):
-    result = {}
+    items = {}
     for qid in qids:
-        result[qid] = {}
+        items[qid] = {}
 
-    items = fetch_statements(qids, media_properties)
-    for qid in items:
-        item = items[qid]
+    statements = fetch_statements(qids, media_properties)
+    for qid in statements:
+        item = statements[qid]
 
         if exists_once(item, "P345"):
-            result[qid]["imdb_id"] = item["P345"][0]
+            items[qid]["imdb_id"] = item["P345"][0]
 
         if exists_once(item, "P1258"):
-            result[qid]["rottentomatoes_id"] = item["P1258"][0]
+            items[qid]["rottentomatoes_id"] = item["P1258"][0]
 
         if exists_once(item, "P2047"):
-            result[qid]["duration"] = int(float(item["P2047"][0]))
+            items[qid]["duration"] = int(float(item["P2047"][0]))
 
         if exists_once(item, "P4947") and "P4983" not in item:
-            result[qid]["tmdb_type"] = "movie"
-            result[qid]["tmdb_id"] = int(item["P4947"][0])
+            items[qid]["tmdb_type"] = "movie"
+            items[qid]["tmdb_id"] = int(item["P4947"][0])
         elif exists_once(item, "P4983") and "P4947" not in item:
-            result[qid]["tmdb_type"] = "tv"
-            result[qid]["tmdb_id"] = int(item["P4983"][0])
+            items[qid]["tmdb_type"] = "tv"
+            items[qid]["tmdb_id"] = int(item["P4983"][0])
 
         if exists_once(item, "P9586") and "P9751" not in item:
-            result[qid]["appletv_id"] = item["P9586"][0]
+            items[qid]["appletv_id"] = item["P9586"][0]
         elif exists_once(item, "P9751") and "P9586" not in item:
-            result[qid]["appletv_id"] = item["P9751"][0]
+            items[qid]["appletv_id"] = item["P9751"][0]
 
     labels = fetch_labels(qids)
     for qid in labels:
-        result[qid]["title"] = labels[qid]
+        items[qid]["title"] = labels[qid]
 
     scores = fetch_tomatometer(qids)
     for qid in scores:
-        result[qid]["tomatometer"] = scores[qid]
+        items[qid]["tomatometer"] = scores[qid]
 
-    return result
+    return items
 
 
 def exists_once(obj, key):
