28e1301a4c7c4fe8b036b34e91cb7dcd9af8029f
Joshua Peek <josh@users.noreply.github.com>
2022-07-24 19:32:31 +0000

    Add clean items script

---
+++ clean_items.py
@@ -0,0 +1,42 @@
+import argparse
+import logging
+import os
+import sqlite3
+
+import requests
+
+from db import items_upsert
+from wikidata import fetch_directed_by
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="Clean up bad database items.",
+    )
+    parser.add_argument("--database", action="store")
+    parser.add_argument("-v", "--verbose", action="count", default=0)
+    args = parser.parse_args()
+
+    loglevel = {
+        0: logging.ERROR,
+        1: logging.WARN,
+        2: logging.INFO,
+        3: logging.DEBUG,
+    }[args.verbose]
+
+    logging.basicConfig(level=loglevel, format="%(message)s")
+    logging.getLogger().setLevel(loglevel)
+
+    con = sqlite3.connect(args.database)
+    con.row_factory = sqlite3.Row
+
+    clean_missing_imdb(con)
+
+
+def clean_missing_imdb(con):
+    con.execute("DELETE FROM items WHERE imdb_id IS NULL")
+    con.commit()
+
+
+if __name__ == "__main__":
+    main()
